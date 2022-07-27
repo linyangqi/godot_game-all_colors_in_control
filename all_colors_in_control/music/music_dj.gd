b975cc80aaa1c4889d75d80e67c22db018198db1
robot_wbw <lin_yang_qi@qq.com>
2022-07-28 07:47:18 +0800

    some update

--- all_colors_in_control/music/music_dj.gd
+++ all_colors_in_control/music/music_dj.gd
@@ -1,7 +1,7 @@
 extends Node
 
 const EVOLVE_NUMBER = 3
-const VOLUME = [-50, -20, -10] #0,1,2 而3就已经质变，保持在-10了
+const VOLUME = [-80, -20, -10] #0,1,2 而3就已经质变，保持在-10了
 
 enum ShipType{Shooter = 0, Knife = 1, Laser = 2, Landminer = 3}
 enum {Shooter = 0, Knife = 1, Laser = 2, Landminer = 3}
